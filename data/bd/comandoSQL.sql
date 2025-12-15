7e149046fe9395c2d0b4cc7e444cce9f8497f931
Iñaki Prieto Barturen <115790143+unamordiditaoso@users.noreply.github.com>
2025-12-16 00:02:37 +0100

    Add files via upload

---
+++ data/bd/comandoSQL.sql
@@ -0,0 +1,17 @@
+CREATE TABLE countries_data (
+    countrynewwb TEXT,
+    codewb TEXT,
+    year INTEGER,
+    pop_adult BIGINT,
+    regionwb24_hi TEXT,
+    incomegroupwb24 TEXT,
+    account_td NUMERIC(5,2),
+
+    country_name_api TEXT,
+    capital_api TEXT,
+    region_api TEXT,
+    subregion_api TEXT,
+
+    flag_url TEXT,
+    local_flag_path TEXT
+);
